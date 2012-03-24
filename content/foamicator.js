75bb480156403241185aa0e7cab7e174057758e8
romaimperator <romaimperator@gmail.com>
2012-03-23 23:27:26 -0500

    The toolbar button is now automagically added to the toolbar.

--- content/foamicator.js
+++ content/foamicator.js
@@ -389,6 +389,13 @@ var Foamicator = {
     this.init_db();
     this.init_listener();
     this.set_status('foamicator idle...');
+
+    if (this.get_b_pref('first_run')) {
+      this.set_b_pref('first_run', false);
+      this.install_button("nav-bar", "foamicator-main-button");
+      // The "addon-bar" is available since Firefox 4
+      this.install_button("addon-bar", "foamicator-main-button");
+    }
   },
 
   /*
@@ -665,6 +672,35 @@ var Foamicator = {
   init_pref: function() {
     this.prefs = Components.classes["@mozilla.org/preferences-service;1"]
                     .getService(Components.interfaces.nsIPrefService).getBranch("extensions.foamicator.");
+  },
+
+  /**
+   * Installs the toolbar button with the given ID into the given
+   * toolbar, if it is not already present in the document.
+   *
+   * @param {string} toolbarId The ID of the toolbar to install to.
+   * @param {string} id The ID of the button to install.
+   * @param {string} afterId The ID of the element to insert after. @optional
+   */
+  install_button: function(toolbarId, id, afterId) {
+      if (!document.getElementById(id)) {
+          var toolbar = document.getElementById(toolbarId);
+
+          // If no afterId is given, then append the item to the toolbar
+          var before = null;
+          if (afterId) {
+              let elem = document.getElementById(afterId);
+              if (elem && elem.parentNode == toolbar)
+                  before = elem.nextElementSibling;
+          }
+
+          toolbar.insertItem(id, before);
+          toolbar.setAttribute("currentset", toolbar.currentSet);
+          document.persist(toolbar.id, "currentset");
+
+          if (toolbarId == "addon-bar")
+              toolbar.collapsed = false;
+      }
   },
 
   /*
